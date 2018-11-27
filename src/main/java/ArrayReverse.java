257768f31ded3488ce083f8d76573afe23154055
RazorWire13 <baneblade13@gmail.com>
2018-11-26 22:44:13 -0800

    reversed the arrays

--- src/main/java/ArrayReverse.java
+++ src/main/java/ArrayReverse.java
@@ -1,27 +1,21 @@
+import java.util.Arrays;
+
 public class ArrayReverse {
 
     public static void main(String[] args) {
-        int[] arr = new int[5];
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 3;
-        arr[3] = 4;
-        arr[4] = 5;
-        //System.out.println(arr);
-        reverseTheArray(arr);
-
-
-        System.out.println(revArr);
+        int[] arr = { 89, 2354, 3546, 23, 10, -923, 823, -12 };
+        System.out.println(Arrays.toString(arr));
+        reverseArray(arr);
     }
 
-    public static int[] reverseTheArray(int[] arr) {
+    public static int[] reverseArray(int[] arr) {
        int n = arr.length-1;
        int[] revArr = new int[arr.length];
        for (int i = 0; i < arr.length; i++) {
            revArr[n] = arr[i];
            n--;
        }
+        System.out.println(Arrays.toString(revArr));
         return revArr;
     }
-
 }
