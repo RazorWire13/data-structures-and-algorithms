33f4f057abc10cf927c684714a517a9979b3ae24
RazorWire13 <baneblade13@gmail.com>
2018-12-19 13:09:12 -0800

    cc17 code complete

--- src/test/java/tree/BinaryTreeTest.java
+++ src/test/java/tree/BinaryTreeTest.java
@@ -8,17 +8,31 @@ public class BinaryTreeTest {
 
     @Test
     public void preOrder() {
+        BinaryTree<String> testTree = new BinaryTree<>();
+        testTree.root = new TreeNode("a", new TreeNode("b", new TreeNode("d", null, null), new TreeNode("e", null, null)), new TreeNode("c", new TreeNode("f", null, null), new TreeNode("g", null, null)));
+        String[] test = {"a", "b", "d", "e", "c", "f", "g"};
+        assertArrayEquals("preOrder returns root-> left-> right branches", test, testTree.preOrder());
     }
 
     @Test
     public void inOrder() {
+        BinaryTree<String> testTree = new BinaryTree<>();
+        testTree.root = new TreeNode("a", new TreeNode("b", new TreeNode("d", null, null), new TreeNode("e", null, null)), new TreeNode("c", new TreeNode("f", null, null), new TreeNode("g", null, null)));
+        String[] test = {"d", "b", "e", "a", "f", "c", "g"};
+        assertArrayEquals("inOrder returns left-> root-> right branches", test, testTree.inOrder());
     }
 
     @Test
     public void postOrder() {
+        BinaryTree<String> testTree = new BinaryTree<>();
+        testTree.root = new TreeNode("a", new TreeNode("b", new TreeNode("d", null, null), new TreeNode("e", null, null)), new TreeNode("c", new TreeNode("f", null, null), new TreeNode("g", null, null)));
+        String[] test = {"d", "e", "b", "f", "g", "c", "a"};
+        assertArrayEquals("postOrder returns left-> right-> root branches", test, testTree.postOrder());
     }
 
     @Test
     public void breadthFirst() {
+        BinaryTree<String> testTree = new BinaryTree<>();
+        testTree.root = new TreeNode("a", new TreeNode("b", new TreeNode("d", null, null), new TreeNode("e", null, null)), new TreeNode("c", new TreeNode("f", null, null), new TreeNode("g", null, null)));
     }
 }
